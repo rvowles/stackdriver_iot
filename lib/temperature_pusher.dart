e7ca2433dda17ec855df3a036c7f4fbae8f87ae2
Richard Vowles <richard@bluetrainsoftware.com>
2019-12-07 21:08:58 +1300

    update api, cleanup
    - update point types with descriptions on enums
    - update temp pusher so it didn't override api + project name

--- lib/temperature_pusher.dart
+++ lib/temperature_pusher.dart
@@ -1,17 +1,10 @@
 import 'package:googleapis/monitoring/v3.dart';
 import 'package:iot_api/api.dart';
-import 'package:logging/logging.dart';
 
 import 'double_pusher.dart';
 
-// have to use 'custom.googleapis.com/' as prefix cannot use anything else
-final _log = Logger('stackdriver');
-
 class TemperaturePusher extends DoublePusher {
-  final MonitoringApi api;
-  final String projectName;
-
-  TemperaturePusher(this.api, this.projectName) : super(api, projectName, 'temperature', 'temperature', 'temperature');
+  TemperaturePusher(MonitoringApi api, String projectName) : super(api, projectName, 'temperature', 'temperature', 'temperature');
 
   @override
   PointType getType() {
